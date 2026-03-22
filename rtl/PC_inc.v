a48cd27897d609dc42c1c829da382f48e37022f6
Manav <iammanav104@gmail.com>
2026-03-22 18:54:27 +0530

    Delete rtl directory

--- rtl/PC_inc.v
+++
@@ -1,9 +0,0 @@
-// a simple adder, that will add 4 to the current PC_value
-
-module PC_inc (
-  input [31:0] fromPC,
-  output [31:0] toPC
-);
-  assign toPC = 4 + fromPC;
-
-endmodule
--- rtl/gatelogic.v
+++
@@ -1,9 +0,0 @@
-module gatelogic (
-  input Branch, zero,
-  output and_out
-);
-  assign and_out = Branch & zero;
-endmodule
-
-// mainly used for the selection purpose in the PC_Branch.
-// is used in the select lines of mux
