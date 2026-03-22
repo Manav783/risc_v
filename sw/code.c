edd23ce19f347816602855ba0b04cb25700770d9
Manav <iammanav104@gmail.com>
2026-03-22 18:57:48 +0530

    Add files via upload

---
+++ sw/code.c
@@ -0,0 +1,9 @@
+int main() {
+  int a = 3;
+  int b = 7;
+  int c = a + b;
+  return c;      // value goes into register a0  - reg10
+}
+
+// FLow of how things work i.e. C to HEX code
+// C - program to GCC compiler to elf file to hex file to HEX Code
